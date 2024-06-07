6c5708d0784e5e8b3d08470b5cb8d0bd22a1dd34
shane <6874@holbertonstudents.com>
2024-06-07 17:11:54 +0200

    task 3

--- scripts.js
+++ scripts.js
@@ -59,198 +59,105 @@ $(document).ready(function () {
 			console.log('il y a une erreur');
 		}
 	});
-	$.ajax({
-		url: "https://smileschool-api.hbtn.info/popular-tutorials",
-		method: "GET",
-		success: function(response) {
-			console.log(response);
-			const allTutos = $('<div class="carousel-inner"></div>');
+});
+$(document).ready(function () {
+	function initializeCarousel(carouselId, apiUrl) {
+		$.ajax({
+			url: apiUrl,
+			method: "GET",
+			success: function(response) {
+				console.log(response);
+				const allTutos = $('<div class="carousel-inner"></div>');
 
-			for (item of response) {
-				const author = item.author;
-				const duration = item.duration;
-				const star = item.star;
-				const id = item.id;
-				const subTitle = item["sub-title"];
-				const title = item.title;
-				const pictureList = item.thumb_url.split('/');
-				const authorPictureList = item.author_pic_url.split('/');
-				const authorPicture = authorPictureList[3];
-				const picture = pictureList[3];
-				let x = 0;
+				for (item of response) {
+					const author = item.author;
+					const duration = item.duration;
+					const star = item.star;
+					const id = item.id;
+					const subTitle = item["sub-title"];
+					const title = item.title;
+					const pictureList = item.thumb_url.split('/');
+					const authorPictureList = item.author_pic_url.split('/');
+					const authorPicture = authorPictureList[3];
+					const picture = pictureList[3];
 
-				const rating = $('<div class="rating"></div>');
-				while (x < 5) {
-					if (x <= star - 1) {
-						const starOn = $('<img src="images/star_on.png" alt="star on" width="15px" height="15px"/>');
-						rating.append(starOn);
-					} else {
-						const starOff = $('<img src="images/star_off.png" alt="star on" width="15px" height="15px"/>');
-						rating.append(starOff);
+					const rating = $('<div class="rating"></div>');
+					for (let x = 0; x < 5; x++) {
+						const starImg = x < star
+							? '<img src="images/star_on.png" alt="star on" width="15px" height="15px"/>'
+							: '<img src="images/star_off.png" alt="star off" width="15px" height="15px"/>';
+						rating.append(starImg);
 					}
-					x += 1;
-				}
 
-				const tuto = $(`
-					<div class="carousel-item">
-						<div class="card">
-							<img src="images/${picture}" class="card-img-top" alt="Video thumbnail"/>
-							<div class="card-img-overlay text-center">
-								<img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay"/>
-							</div>
-							<div class="card-body">
-								<h5 class="card-title font-weight-bold">${title}</h5>
-								<p class="card-text text-muted">${subTitle}</p>
-								<div class="creator d-flex align-items-center">
-									<img src="images/${authorPicture}" alt="Creator of Video" width="30px" class="rounded-circle"/>
-									<h6 class="pl-3 m-0 main-color">${author}</h6>
+					const tuto = $(`
+						<div class="carousel-item">
+							<div class="card">
+								<img src="images/${picture}" class="card-img-top" alt="Video thumbnail"/>
+								<div class="card-img-overlay text-center">
+									<img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay"/>
 								</div>
-								<div class="info pt-3 d-flex justify-content-between">
-									${rating.prop('outerHTML')}
-									<span class="main-color">${duration}</span>
-								</div>
-							</div>
-						</div>
-					</div>
-				`);
-
-				allTutos.append(tuto);
-			}
-
-			$('#carouselExampleControls2').empty().append(allTutos);
-
-
-			// Initialize Slick Carousel
-			$('#carouselExampleControls2 .carousel-inner').slick({
-				slidesToShow: 4,
-				slidesToScroll: 1,
-				arrows: true,
-				prevArrow: '<button class="slick-prev arrow-left"><img src="images/arrow_black_left.png" alt="Previous" aria-hidden="true"/><span class="sr-only">Previous</span></button>',
-				nextArrow: '<button class="slick-next arrow-right"><img src="images/arrow_black_right.png" alt="Next" aria-hidden="true"/><span class="sr-only">Next</span></button>',
-				responsive: [
-					{
-						breakpoint: 1024,
-						settings: {
-							slidesToShow: 3,
-							slidesToScroll: 1,
-						}
-					},
-					{
-						breakpoint: 768,
-						settings: {
-							slidesToShow: 2,
-							slidesToScroll: 1,
-						}
-					},
-					{
-						breakpoint: 576,
-						settings: {
-							slidesToShow: 1,
-							slidesToScroll: 1,
-						}
-					}
-				]
-			});
-		},
-		error: function() {
-			console.log("error");
-		}
-	});
-	$.ajax({
-		url: "https://smileschool-api.hbtn.info/latest-videos",
-		method: "GET",
-		success: function(response) {
-			console.log(response);
-			const allTutos = $('<div class="carousel-inner"></div>');
-
-			for (item of response) {
-				const author = item.author;
-				const duration = item.duration;
-				const star = item.star;
-				const id = item.id;
-				const subTitle = item["sub-title"];
-				const title = item.title;
-				const pictureList = item.thumb_url.split('/');
-				const authorPictureList = item.author_pic_url.split('/');
-				const authorPicture = authorPictureList[3];
-				const picture = pictureList[3];
-				let x = 0;
-
-				const rating = $('<div class="rating"></div>');
-				while (x < 5) {
-					if (x <= star - 1) {
-						const starOn = $('<img src="images/star_on.png" alt="star on" width="15px" height="15px"/>');
-						rating.append(starOn);
-					} else {
-						const starOff = $('<img src="images/star_off.png" alt="star on" width="15px" height="15px"/>');
-						rating.append(starOff);
-					}
-					x += 1;
-				}
-
-				const tuto = $(`
-					<div class="carousel-item">
-						<div class="card">
-							<img src="images/${picture}" class="card-img-top" alt="Video thumbnail"/>
-							<div class="card-img-overlay text-center">
-								<img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay"/>
-							</div>
-							<div class="card-body">
-								<h5 class="card-title font-weight-bold">${title}</h5>
-								<p class="card-text text-muted">${subTitle}</p>
-								<div class="creator d-flex align-items-center">
-									<img src="images/${authorPicture}" alt="Creator of Video" width="30px" class="rounded-circle"/>
-									<h6 class="pl-3 m-0 main-color">${author}</h6>
-								</div>
-								<div class="info pt-3 d-flex justify-content-between">
-									${rating.prop('outerHTML')}
-									<span class="main-color">${duration}</span>
+								<div class="card-body">
+									<h5 class="card-title font-weight-bold">${title}</h5>
+									<p class="card-text text-muted">${subTitle}</p>
+									<div class="creator d-flex align-items-center">
+										<img src="images/${authorPicture}" alt="Creator of Video" width="30px" class="rounded-circle"/>
+										<h6 class="pl-3 m-0 main-color">${author}</h6>
+									</div>
+									<div class="info pt-3 d-flex justify-content-between">
+										${rating.prop('outerHTML')}
+										<span class="main-color">${duration}</span>
+									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-				`);
+					`);
 
-				allTutos.append(tuto);
+					allTutos.append(tuto);
+				}
+
+				$(carouselId).empty().append(allTutos);
+
+				// Initialize Slick Carousel
+				$(`${carouselId} .carousel-inner`).slick({
+					slidesToShow: 4,
+					slidesToScroll: 1,
+					arrows: true,
+					prevArrow: '<button class="slick-prev arrow-left"><img src="images/arrow_black_left.png" alt="Previous" aria-hidden="true"/><span class="sr-only">Previous</span></button>',
+					nextArrow: '<button class="slick-next arrow-right"><img src="images/arrow_black_right.png" alt="Next" aria-hidden="true"/><span class="sr-only">Next</span></button>',
+					responsive: [
+						{
+							breakpoint: 1024,
+							settings: {
+								slidesToShow: 3,
+								slidesToScroll: 1,
+							}
+						},
+						{
+							breakpoint: 768,
+							settings: {
+								slidesToShow: 2,
+								slidesToScroll: 1,
+							}
+						},
+						{
+							breakpoint: 576,
+							settings: {
+								slidesToShow: 1,
+								slidesToScroll: 1,
+							}
+						}
+					]
+				});
+			},
+			error: function() {
+				console.log("error");
 			}
+		});
+	}
 
-			$('#carouselExampleControls3').empty().append(allTutos);
+	// Initialize both carousels
 
+	initializeCarousel('#carouselExampleControls2', "https://smileschool-api.hbtn.info/popular-tutorials");
+	initializeCarousel('#carouselExampleControls3', "https://smileschool-api.hbtn.info/latest-videos");
 
-			// Initialize Slick Carousel
-			$('#carouselExampleControls3 .carousel-inner').slick({
-				slidesToShow: 4,
-				slidesToScroll: 1,
-				arrows: true,
-				prevArrow: '<button class="slick-prev arrow-left"><img src="images/arrow_black_left.png" alt="Previous" aria-hidden="true"/><span class="sr-only">Previous</span></button>',
-				nextArrow: '<button class="slick-next arrow-right"><img src="images/arrow_black_right.png" alt="Next" aria-hidden="true"/><span class="sr-only">Next</span></button>',
-				responsive: [
-					{
-						breakpoint: 1024,
-						settings: {
-							slidesToShow: 3,
-							slidesToScroll: 1,
-						}
-					},
-					{
-						breakpoint: 768,
-						settings: {
-							slidesToShow: 2,
-							slidesToScroll: 1,
-						}
-					},
-					{
-						breakpoint: 576,
-						settings: {
-							slidesToShow: 1,
-							slidesToScroll: 1,
-						}
-					}
-				]
-			});
-		},
-		error: function() {
-			console.log("error");
-		}
-	});
 });
